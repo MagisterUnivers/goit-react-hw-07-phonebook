1943d50d6b89fa977fb525c10f1a6177c70d16c8
Andrey Shaposhnikov <droniiiandruxa@gmail.com>
2023-05-03 00:29:30 +0300

    HW8 menthor check2

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -32,7 +32,7 @@ export const deleteContact = createAsyncThunk(
   async (contactId, thunkAPI) => {
     try {
       const response = await axios.delete(`/contacts/${contactId}`);
-      thunkAPI.dispatch(fetchContacts());
+      // thunkAPI.dispatch(fetchContacts());
       return response.data;
     } catch (e) {
       return thunkAPI.rejectWithValue(e.message);
